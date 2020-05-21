338a551e9a36230abb9dfdabf92df43ab400189f
Aaron Schuyler <asky28@gmail.com>
2020-05-21 16:24:03 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,9 +3,15 @@ class Song
   @@all = []
   def initialize(name)
     @name = name
-    @@all << self
+    self.save
   end
   def self.all
     @@all
+  end
+  def save
+    @@all << self
+  end
+  def self.destroy_all
+    @@all = []
   end
 end
